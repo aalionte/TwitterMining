8b6182a03571bfc91a3beb199ac64161df07e16e
LE Minh Quoc <leminhquoc@gmail.com>
2015-11-18 15:43:18 +0100

    initial code
    @lmquoc

--- src/twittermining/TwitterMining.java
+++ src/twittermining/TwitterMining.java
@@ -1,10 +1,19 @@
 package twittermining;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class TwitterMining {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-
+		FileWriter myTextFile = new FileWriter("E:/TwitterMining.txt", true);
+		
+		BufferedWriter myBuffer = new BufferedWriter(myTextFile);
+		myBuffer.write("Hello World");
+		myBuffer.write("Hello World 2");		
+		System.out.println("FINISHED");
 	}
 
 }
