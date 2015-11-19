65d45ad922d065ba04cfd387118f6313f83ef2ba
LE Minh Quoc <leminhquoc@gmail.com>
2015-11-19 14:05:18 +0100

    refine Huy's code
    Anh đã refine lại code của em cho tinh gọn hơn. Và thêm điền kiện vào if
    để code cho kết quả chạy ra chính xác hơn.

--- src/twittermining/TwitterMining.java
+++ src/twittermining/TwitterMining.java
@@ -21,20 +21,26 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 public class TwitterMining {
+	
+	// For QUOC: CHANGE HERE
+	private static String userDataPath = "E:/TwitterMining/data/";
+	private static String userGitPath = "E:/Github/TwitterMining/";
+	
+	// For HUY: CHANGE HERE
+//	private static String userDataPath = "";
+//	private static String userGitPath = "";
 
-	// FILE PATH: CHANGE HERE
-	private static String fileTxtOutput = "E:/Github/TwitterMining/TwitterMining.txt";
-	private static String fileEdgeCalculation = "E:/Github/TwitterMining/EdgeCalculation.txt";
-	private static String targetFolder = "E:/TwitterMining/data/Oscars/";
+	// FILE PATH 
+	private static String [] folderName = {"NewYorkOneWeek", "Oscars", "ParisSearchFeb", "ParisSearchJan"};
+	private static String targetFolder =  userDataPath + folderName[1]; // CHANGE idx HERE !!!!!!!
+	
+	private static String fileTxtOutput = userGitPath + "TwitterMining.txt";
+	private static String fileEdgeCalculation = userGitPath + "EdgeCalculation.txt";
+
 	
 	// GLOBAL VARIABLES
 	static List<String> arrayHashtags = new ArrayList<String>();
-	static int counter = 0;
-
-	// private static String fileJsonInput =
-	// "/Users/huydinh/Cours/TPT32/Project/dataAthensWeek/NewYorkOneWeek/NewYork-2015-2-23";
-	// private static String fileTxtOutput =
-	// "/Users/huydinh/Documents/workspace/TwitterMining/data/processedData/TwitterMining.txt";
+	static int counter = 0;	
 
 	public static void main(String[] args) throws IOException, ParseException {		
 		readFileFromFolder(targetFolder);		
@@ -126,62 +132,74 @@ public class TwitterMining {
 		BufferedWriter myWriter = new BufferedWriter(new FileWriter(fileEdgeCalculation)); 
 		
 		int count = 0;
-		SimpleWeightedGraph<String, DefaultWeightedEdge> hashtagGraph = new SimpleWeightedGraph<String, DefaultWeightedEdge>(
-				DefaultWeightedEdge.class);
-		Set<Set<String>> allHashtagSet = new HashSet<Set<String>>();
-
+		int vertexCount = 0;
+		SimpleWeightedGraph<String, DefaultWeightedEdge> hashtagGraph = new SimpleWeightedGraph<String, DefaultWeightedEdge>(DefaultWeightedEdge.class);
+	
 		// Read the input file (each line is a list of hashtags we had in each Twit)
-		List<String> lines = Files.readAllLines(Paths.get(fileTxtOutput),
-				StandardCharsets.ISO_8859_1);
+		List<String> lines = Files.readAllLines(Paths.get(fileTxtOutput), StandardCharsets.ISO_8859_1);
 		for (int i = 0; i < lines.size(); i++) {
+			
+			// hashtagList contains hashtags of ONE Twit 
 			List<String> hashtagList = Arrays.asList(lines.get(i).split(" "));
-			for (int u = 0; u < hashtagList.size(); u++){
-				// Replace the hashtag at idx i by its lower case.
+			
+			// Replace the hashtag at idx i by its lower case
+			for (int u = 0; u < hashtagList.size(); u++) {				
 				hashtagList.set(u, hashtagList.get(u).toLowerCase());
 			}
-
-			Set<String> hashtagSet = new HashSet<String>(hashtagList);
-			if (!allHashtagSet.contains(hashtagSet)) {
-				count++;
-				allHashtagSet.add(hashtagSet);
-				hashtagList = new ArrayList<String>(hashtagSet);
-
-				for (int u = 0; u < hashtagList.size(); u++)
-					if (!hashtagGraph.containsVertex(hashtagList.get(u)))
-						hashtagGraph.addVertex(hashtagList.get(u));
-				
-				for (int u = 0; u < hashtagList.size() - 1; u++)
-					for (int v = u + 1; v < hashtagList.size(); v++) {
-						if (!hashtagGraph.containsEdge(hashtagList.get(u),hashtagList.get(v))){
-							if (hashtagGraph.containsEdge(hashtagList.get(v),hashtagList.get(u))){
-								// Just to check if there is an error on duplication of A --> B and B --> A
-								System.out.println("CHET MAY NHA CON **********************************");
-							}
-							hashtagGraph.addEdge(hashtagList.get(u),hashtagList.get(v));
-						}
-						else {
-							DefaultWeightedEdge edge = hashtagGraph.getEdge(
-									hashtagList.get(u), hashtagList.get(v));
-							hashtagGraph.setEdgeWeight(edge,
-									hashtagGraph.getEdgeWeight(edge) + 1.0);
-						}
-					}
+			
+			// Add new (unique) Vertex to the Graph
+			for (int u = 0; u < hashtagList.size(); u++){
+				if (!hashtagGraph.containsVertex(hashtagList.get(u))){
+					vertexCount++;
+					hashtagGraph.addVertex(hashtagList.get(u));
+				}					
 			}
+			
+			// Create the edges from each pair of hashtags in ONE Twit
+			if(hashtagList.size() > 2){
+				for (int u = 0; u < hashtagList.size() - 1; u++){
+					for (int v = u + 1; v < hashtagList.size(); v++) {
+						// Make sure that 2 hashtags are different
+						if(!hashtagList.get(u).equalsIgnoreCase(hashtagList.get(v))){
+							// Check if the Graph has this edge or not
+							if (!hashtagGraph.containsEdge(hashtagList.get(u),hashtagList.get(v))) {
+								if (hashtagGraph.containsEdge(hashtagList.get(v), hashtagList.get(u))) {
+									// Just to check if there is an error on duplication of A --> B and B --> A
+									System.out.println("Directed edge detected **********************************");
+								}									
+								count++;
+								hashtagGraph.addEdge(hashtagList.get(u), hashtagList.get(v));
+								
+								// Initialize the weight
+								DefaultWeightedEdge edge = hashtagGraph.getEdge(hashtagList.get(u), hashtagList.get(v));
+								double Weight = 1.0;						
+								hashtagGraph.setEdgeWeight(edge, Weight);
+							} 
+							// Add 1 to weight if edge occurs 1 more time
+							else {
+								DefaultWeightedEdge edge = hashtagGraph.getEdge(hashtagList.get(u), hashtagList.get(v));
+								double newWeight = hashtagGraph.getEdgeWeight(edge) + 1.0;						
+								hashtagGraph.setEdgeWeight(edge, newWeight);
+							}
+						}						
+					}
+				}
+			}							
 		}
-
-		//System.out.println(hashtagGraph.toString());
-		String output = "";
 		
+		String output = "";		
 		for (DefaultWeightedEdge edge : hashtagGraph.edgeSet()) {
-			if (hashtagGraph.getEdgeWeight(edge) > 10.0) {
+			if (hashtagGraph.getEdgeWeight(edge) > 30.0) {				
+				// Write to the output file
 				output = edge.toString() + " " + hashtagGraph.getEdgeWeight(edge);				
 				myWriter.write(output);
 				myWriter.newLine();
 			}
 		}
-		
+//		
 		myWriter.close();
 		
+		System.out.println(vertexCount + " vertex");
 		System.out.println(count + " edges");
 		System.out.println("FINISHED");
 		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
